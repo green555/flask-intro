56447ed8117afa194f279575253a1b9f16a147ab
Wei Wei <weiweibcm@gmail.com>
2022-03-03 21:30:16 -0800

    completed

--- server.py
+++ server.py
@@ -18,14 +18,21 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+      <html>
+        Hi! This is the home page.
+        <a href="http://localhost:5002/hello">Hello</a>
+      </html>
+
+    """
 
 
 @app.route('/hello')
 def say_hello():
-    """Say hello and prompt for user's name."""
+    """Say hello and prompt for users name."""
 
-    return """
+    return f"""
     <!doctype html>
     <html>
       <head>
@@ -33,8 +40,19 @@ def say_hello():
       </head>
       <body>
         <h1>Hi There!</h1>
-        <form action="/greet">
+        <form action="/greet" method="GET">
           What's your name? <input type="text" name="person">
+          <br>
+            Select your compliment!
+          <br>
+          <select name="AWE">           
+            <option value = {AWESOMENESS[0]}>{AWESOMENESS[0]}</option>
+            <option value = {AWESOMENESS[1]}>{AWESOMENESS[1]}</option>
+            <option value = {AWESOMENESS[2]}>{AWESOMENESS[2]}</option>
+            <option value = {AWESOMENESS[3]}>{AWESOMENESS[3]}</option>
+            <option value = {AWESOMENESS[4]}>{AWESOMENESS[4]}</option>
+            <option value = {AWESOMENESS[5]}>{AWESOMENESS[5]}</option>
+          
           <input type="submit" value="Submit">
         </form>
       </body>
@@ -48,8 +66,8 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
-
+    # compliment = choice(AWESOMENESS)
+    compliment = AWESOMENESS[int(request.args.get("AWE"))]
     return f"""
     <!doctype html>
     <html>
@@ -66,4 +84,4 @@ def greet_person():
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5002)
